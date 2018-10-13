53e8db3ea03f95e3662d0c093cbf965f2b9807ac
Przemysław Chojnowski <przemyslaw.chojnowski1995@gmail.com>
2018-10-13 15:13:19 +0200

    Redux devtools added to project

--- DziennikSportowca/app/src/_helpers/store.js
+++ DziennikSportowca/app/src/_helpers/store.js
@@ -1,14 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import rootReducer from '../_reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import rootReducer from "../_reducers";
 
 const loggerMiddleware = createLogger();
 
-export const store = createStore(
-    rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunkMiddleware, loggerMiddleware)
+    // other store enhancers if any
 );
+export const store = createStore(rootReducer, enhancer);
+
+// export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
